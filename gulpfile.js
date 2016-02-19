37927f9da172c37e73bcf322e222746730959515
jiangzhu <Monster000@jiangzhudeMacBook-Pro.local>
2016-02-19 23:26:23 +0800

    增加按钮基础样式和其他颜色的按钮

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 
-gulp.task('less', function () {
-  return gulp.src('./src/less/app.less')
-    .pipe(less({
-      //paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('less', function() {
+    return gulp.src('./src/less/app.less')
+        .pipe(less({
+            //paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('default', function() {
-
+    var watcher = gulp.watch('src/**/*.less', ['less']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 })
